1126c6512800235d5b753c8d2104a2d0024f9901
Magnus Forsman <forsman.magnus@gmail.com>
2024-05-07 12:56:32 +0200

    Add project files.

---
+++ FunctionRESTAPI/Migrations/20240506123421_first.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FunctionRESTAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Owner = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
---
+++ FunctionRESTAPI/Program.cs
@@ -0,0 +1,27 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using FunctionRESTAPI;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices((hostContext, services) =>
+    {
+        string sqlConnectionString = Environment.GetEnvironmentVariable("SqlConnectionStringLabb3");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(sqlConnectionString);
+        });
+
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .Build();
+
+await host.RunAsync();
